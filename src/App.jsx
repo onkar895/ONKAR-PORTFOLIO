8cff1c2eaadcfa0b587d3f801437d0d28b820609
Onkar Karale <onkarkarale4@gmail.com>
2024-02-15 19:22:19 +0530

    Made the setup for the project using Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-400 text-white font-bold mx-72 p-2 rounded-lg shadow-2xl shadow-black'>
+        HIIII, WELCOME TO MY PORTFOLIO WEBSITE
+      </div>
+    </>
+  )
+}
+
+export default App
