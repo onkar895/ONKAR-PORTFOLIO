a4ec86901ecc6e376a166117dc14f24c983a9570
Onkar Karale <onkarkarale4@gmail.com>
2024-02-16 02:46:50 +0530

    modified NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -7,10 +7,10 @@ import Character from '../assets/Character.png'
 
 const NavBar = () => {
   return (
-    <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
+    <nav data-aos="fade-up" className='fixed bottom-4 lg:bottom-8 w-full overflow-hidden z-50'>
       <div className='container mx-auto'>
         {/* nav inner */}
-        <div className='w-full bg-black/10 h-[70px] backdrop-blur rounded-full max-w-[560px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50'>
+        <div className='w-full bg-black/10 h-[70px] backdrop-blur rounded-full max-w-[560px] mx-auto px-5 max-sm:px-3 flex justify-between items-center text-2xl text-white/50'>
           <div className="w-[42px] h-[42px] flex items-center justify-center flex-short rounded-full custom-border box-shadow bg-white group overflow-hidden hover:overflow-hidden cursor-pointer">
             <div className="scale-[1.4] transition group-hover:scale-[1.7] w-[32px]">
               <img src={Character} alt="logo" className='rounded-full' />
@@ -28,7 +28,7 @@ const NavBar = () => {
           <Link className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center hover:text-white'>
             <BsBriefcase />
           </Link>
-          <button className='btn text-sm cursor-pointer px-9 py-[10px]'>
+          <button className='btn text-sm cursor-pointer px-9 py-[10px] max-sm:hidden'>
             <span className='animate-pulse'>Hire Me</span>
           </button>
         </div>
