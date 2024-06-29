4e65157e8f74bcf6fe27910c0a90ce1a210b63f1
Onkar Karale <onkarkarale4@gmail.com>
2024-06-29 16:38:10 +0530

    Updated the required Components for more better UI and UX

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,48 +7,28 @@ import { FaReact, FaBootstrap, FaGithub, FaGitAlt } from "react-icons/fa";
 
 const Skills = () => {
   return (
-    <div className="lg:w-[33.33%] w-full shadow-inner shadow-cyan-900 hover:shadow hover:shadow-cyan-300  bg-transparent backdrop-blur rounded-xl py-6 md:px-7 overflow-hidden cursor-pointer hover:scale-95 transition-all duration-300">
-      <h3 data-aos="fade-up" data-aos-duration="2000" className="font-bold lg:text-lg text-2xl text-gradient tracking-wide text-center">Skills</h3>
-      <div data-aos="fade-up" data-aos-duration="2000" className="flex md:gap-x-10 gap-x-20 gap-y-10 flex-wrap lg:py-6 py-10 max-sm:mx-auto justify-center">
-        <div className='bg-white p-2 rounded-xl'>
-          <IoLogoHtml5 className='text-orange-500 text-3xl' />
-        </div>
-        <div className='bg-white p-2 rounded-xl'>
-          <IoLogoCss3 className='text-blue-600 text-3xl' />
-        </div>
-        <div className='bg-[#FDEA31] p-2 rounded-xl'>
-          <IoLogoJavascript className='text-black text-3xl' />
-        </div>
-        <div className='bg-black shadow-inner shadow-gray-400 p-2 rounded-xl'>
-          <FaReact className='text-[#4dbfd6] text-3xl font-bolder' />
-        </div>
-        <div className='bg-white p-2 rounded-xl'>
-          <SiRedux className='text-[#6C4EB0] text-3xl' />
-        </div>
-        <div className='bg-white p-2 rounded-xl'>
-          <SiTailwindcss className='text-[#54c2d8] text-3xl' />
-        </div>
-        <div className='bg-white p-2 rounded-xl'>
-          <SiMui className='text-[#007FFF] text-3xl' />
-        </div>
-        <div className='bg-white p-2 rounded-xl'>
-          <FaBootstrap className='text-[#9E64F2] text-3xl' />
-        </div>
-        <div className='bg-white p-2 rounded-xl'>
-          <FaGitAlt className='text-[#F34F29] text-3xl' />
-        </div>
-        <div className='bg-white p-2 rounded-xl'>
-          <FaGithub className='text-black text-3xl' />
-        </div>
-        <div className='bg-white p-2 rounded-xl'>
-          <FaGitlab className='text-[#E24329] text-3xl' />
-        </div>
-        <div className='bg-white p-2 rounded-xl'>
-          <SiFirebase className='text-[#FFA000] text-3xl' />
-        </div>
-        <div className='bg-black shadow-inner shadow-gray-400 p-2 rounded-xl'>
-          <SiVercel className='text-white text-3xl font-bolder' />
-        </div>
+    <div className="lg:w-[33.33%] w-full shadow-inner shadow-cyan-600 hover:shadow-lg hover:shadow-cyan-300 bg-gradient-to-br from-transparent to-black/50 backdrop-blur rounded-xl py-6 md:px-7 overflow-hidden cursor-pointer hover:scale-95 transition-transform duration-300">
+      <h3 data-aos="fade-up" data-aos-duration="2000" className="font-bold lg:text-lg text-2xl text-center text-gradient tracking-wide">Skills</h3>
+      <div data-aos="fade-up" data-aos-duration="2000" className="flex md:gap-x-10 gap-x-6 gap-y-10 flex-wrap lg:py-6 py-10 justify-center">
+        {[
+          { icon: <IoLogoHtml5 className='text-orange-500 text-3xl' />, bg: 'bg-white' },
+          { icon: <IoLogoCss3 className='text-blue-600 text-3xl' />, bg: 'bg-white' },
+          { icon: <IoLogoJavascript className='text-black text-3xl' />, bg: 'bg-[#FDEA31]' },
+          { icon: <FaReact className='text-[#4dbfd6] text-3xl' />, bg: 'bg-black' },
+          { icon: <SiRedux className='text-[#6C4EB0] text-3xl' />, bg: 'bg-white' },
+          { icon: <SiTailwindcss className='text-[#54c2d8] text-3xl' />, bg: 'bg-white' },
+          { icon: <SiMui className='text-[#007FFF] text-3xl' />, bg: 'bg-white' },
+          { icon: <FaBootstrap className='text-[#9E64F2] text-3xl' />, bg: 'bg-white' },
+          { icon: <FaGitAlt className='text-[#F34F29] text-3xl' />, bg: 'bg-white' },
+          { icon: <FaGithub className='text-black text-3xl' />, bg: 'bg-white' },
+          { icon: <FaGitlab className='text-[#E24329] text-3xl' />, bg: 'bg-white' },
+          { icon: <SiFirebase className='text-[#FFA000] text-3xl' />, bg: 'bg-white' },
+          { icon: <SiVercel className='text-white text-3xl' />, bg: 'bg-black' },
+        ].map(({ icon, bg }, index) => (
+          <div key={index} className={`p-2 rounded-xl transition-transform duration-300 transform hover:scale-110 shadow-md ${bg}`}>
+            {icon}
+          </div>
+        ))}
       </div>
     </div>
   )
--- src/Components/WorkData.jsx
+++ src/Components/WorkData.jsx
@@ -11,14 +11,14 @@ const WorkData = ({ info }) => {
     <div data-aos="fade-up" data-aos-duration="2000"
       className="lg:w-[26.8vw] lg:h-[55vh] w-[85vw] h-[42vh] md:w-[50vw] md:h-[40vh] cursor-pointer relative overflow-hidden group"
     >
-      <div className='lg:w-[26.8vw] lg:h-[38vh] w-[85vw] h-[28vh] md:w-[50vw] md:h-[28vh] flex items-center justify-center shadow-inner shadow-cyan-800 backdrop-blur-3xl rounded-t-xl overflow-hidden hover:overflow-hidden cursor-pointer relative group'>
+      <div className='lg:w-[26.8vw] lg:h-[38vh] w-[85vw] h-[28vh] md:w-[50vw] md:h-[28vh] flex items-center justify-center bg-gradient-to-br from-transparent to-white/30 shadow-inner shadow-cyan-700 backdrop-blur-3xl rounded-t-xl overflow-hidden hover:overflow-hidden cursor-pointer relative group'>
         <img
           src={info.img}
           alt={info.title}
-          className="lg:w-[21vw] lg:h-[26vh] w-[72vw] h-[21vh] md:w-[40vw] md:h-[20vh] scale-[1.1] object-fit group-hover:scale-[1.4] rounded-xl transition-all ease-in-out duration-1000 shadow-lg shadow-cyan-600"
+          className="lg:w-[20vw] lg:h-[25vh] w-[72vw] h-[22vh] md:w-[40vw] md:h-[20vh] scale-[1.1] object-fit group-hover:scale-[1.4] rounded-xl transition-all ease-in-out duration-500 shadow-lg shadow-cyan-600"
         />
         <div className="absolute top-0 left-0 w-full h-full">
-          <div className="bg-black/70 w-full h-full flex justify-center gap-6 items-center opacity-0 hover:opacity-100 transition-all ease-in-out duration-1000">
+          <div className="bg-black/80 w-full h-full flex justify-center gap-6 items-center opacity-0 hover:opacity-100 transition-all ease-in-out duration-1000">
             <Link
               to={info.git}
               className="btn px-5 py-[6px] border-2  hover:brightness-200 transition-all ease-in-out duration-500 hover:font-extrabold hover:text-black rounded-full cursor-pointer text-white text-sm"
