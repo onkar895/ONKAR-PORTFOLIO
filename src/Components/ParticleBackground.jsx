7906f64f5ff7664c1bb6bba68aaa0fccbb5622ee
Onkar Karale <onkarkarale4@gmail.com>
2024-07-10 20:34:38 +0530

    Updated WorkData, ParticleBackground and index.css

--- src/Components/ParticleBackground.jsx
+++ src/Components/ParticleBackground.jsx
@@ -4,17 +4,15 @@ const ParticleBackground = () => {
   return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none animate-pulse">
       {Array.from({ length: 80 }).map((_, i) => {
-        const type = i % 2 === 0 ? 'particle2' : 'particle';
-        const color = i % 2 === 0 ? 'pink' : 'white';
         const duration = 4 + Math.random() * 10;
         return (
           <div
             key={i}
-            className={type}
+            className="particle"
             style={{
               top: `${Math.random() * 100}%`,
               left: `${Math.random() * 100}%`,
-              '--color': color,
+              '--color': 'white',
               '--duration': `${duration}s`,
               animationDelay: `${Math.random() * 5}s`,
             }}
--- src/Components/WorkData.jsx
+++ src/Components/WorkData.jsx
@@ -9,13 +9,13 @@ const WorkData = ({ info }) => {
 
   return (
     <div data-aos="fade-up" data-aos-duration="2000"
-      className="relative lg:w-[26.8vw] lg:h-[60vh] w-[85vw] h-[42vh] md:w-[50vw] md:h-[40vh] cursor-pointer shadow-inner shadow-cyan-900  bg-gradient-to-b from-[#14141b] to-white/10 overflow-hidden backdrop-blur rounded-xl group"
+      className="relative lg:w-[26.8vw] lg:h-[60vh] w-[85vw] h-[42vh] md:w-[65vw] md:h-[50vh] flex flex-col items-center justify-center cursor-pointer shadow-inner shadow-cyan-900 bg-gradient-to-b from-[#14141b] to-white/10 overflow-hidden backdrop-blur rounded-xl group"
     >
-      <div className='relative lg:w-[26.8vw] lg:h-[42vh] w-[85vw] h-[28vh] md:w-[50vw] md:h-[28vh] flex items-center justify-center overflow-hidden hover:overflow-hidden cursor-pointer group'>
+      <div className='relative lg:w-[26.8vw] lg:h-[40vh] w-[85vw] h-[28vh] md:w-[62vw] md:h-[40vh] flex items-center justify-center overflow-hidden hover:overflow-hidden cursor-pointer group'>
         <img
           src={info.img}
           alt={info.title}
-          className="lg:h-[42vh] h-[32vh] md:h-[30vh] object-contain group-hover:scale-[2] rounded-xl transition-transform ease-in-out duration-300 img1"
+          className="lg:h-[44vh] h-[36vh] object-contain group-hover:scale-[2] rounded-xl transition-transform ease-in-out duration-300 img1"
         />
         <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center gap-4 bg-black/80 opacity-0 group-hover:opacity-100 transition-opacity duration-500">
           <Link
@@ -38,7 +38,7 @@ const WorkData = ({ info }) => {
           </Link>
         </div>
       </div>
-      <div className='flex flex-col py-2 text-center max-sm:gap-1 shadow-inner shadow-white/10'>
+      <div className='flex flex-col py-2 text-center max-sm:gap-1 shadow-inner shadow-white/10 w-full'>
         <p className='font-bold text-gray-300 max-sm:text-xl'>{info.title}</p>
         <p className='text-xs md:font-bold text-gray-400'>{info.desc}</p>
         <div className='text-xs md:font-bold text-gray-400 md:mt-1'>
