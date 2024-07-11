42ddf6658d137ff4adcbea1bfc72ea13c7b9c572
Onkar Karale <onkarkarale4@gmail.com>
2024-07-11 18:14:06 +0530

    Updated the Components, index.css for more better UI and UX

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,32 +1,23 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from 'react'
-import { BiHomeAlt, BiUser } from 'react-icons/bi'
-import { BsBriefcase } from 'react-icons/bs'
+import React, { useState, useEffect } from 'react';
+import { BiSolidUser } from "react-icons/bi";
+import { BsBriefcaseFill, BsFillPersonVcardFill } from 'react-icons/bs';
 import { useLocation } from 'react-router-dom';
-import { LuContact } from "react-icons/lu";
+import { MdHomeFilled } from 'react-icons/md';
 import { NavLink } from 'react-router-dom';
-import Character from '../assets/Character.png'
+import Character from '../assets/Character.png';
 
 const NavBar = () => {
-
   const [showNavBar, setShowNavBar] = useState(false);
-  const [lastScrollY, setLastScrollY] = useState(0);
   const location = useLocation();
-
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
-
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY === 0) {
-        setShowNavBar(false);
-      } else {
-        setShowNavBar(true);
-      }
-      setLastScrollY(window.scrollY);
+      setShowNavBar(window.scrollY !== 0);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -34,60 +25,45 @@ const NavBar = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [lastScrollY]);
+  }, []);
 
-  const IconStyle = "w-[40px] h-[40px] flex items-center justify-center flex-short rounded-full border-2 box-shadow border border-white/20 hover:border-white/40 transition-all duration-500 shadow-2xl group overflow-hidden hover:overflow-hidden cursor-pointer"
+  const iconStyle = 'w-10 h-10 flex items-center justify-center rounded-full border-2 border-white/20 hover:border-white/40 hover:text-white transition-all ease-in-out hover:overflow-hidden duration-500 shadow-2xl group overflow-hidden hover:overflow-hidden cursor-pointer';
 
   return (
     <nav className={`fixed bottom-2 max:sm:bottom-1 w-full overflow-hidden z-50 transition-transform duration-500 ${showNavBar ? 'translate-y-0' : 'translate-y-full'}`}>
-      <div className='container mx-auto'>
-        {/* nav inner */}
-        <div className='w-full bg-[rgba(30,23,53,0.4)] backdrop-blur-xl shadow-2xl backdrop-contrast-125 h-[65px] rounded-full max-w-[500px] mx-auto px-5 max-sm:px-3 flex justify-between items-center text-2xl text-white/70'>
-          <div className="w-[40px] h-[40px] flex items-center justify-center flex-short rounded-full custom-border box-shadow bg-white group overflow-hidden hover:overflow-hidden cursor-pointer">
-            <div
-              className="scale-[1.3] transition group-hover:scale-[1.6] w-[32px]">
-              <img src={Character} alt="logo" className='rounded-full' />
+      <div className="container mx-auto">
+        <div className="w-full bg-[rgba(30,23,53,0.4)] backdrop-blur-xl shadow-2xl backdrop-contrast-125 h-16 rounded-full max-w-[500px] mx-auto px-5 max-sm:px-3 flex justify-between items-center text-2xl text-white/70">
+          <div className="w-10 h-10 flex items-center justify-center rounded-full custom-border box-shadow bg-white group overflow-hidden hover:overflow-hidden cursor-pointer">
+            <div className="scale-[1.3] transition group-hover:scale-[1.6]">
+              <img src={Character} alt="logo" className="rounded-full" />
             </div>
           </div>
-          <div className={IconStyle}>
-            <NavLink
-              to='/'
-              activeClass='active'
-              className='cursor-pointer flex items-center justify-center hover:text-white'>
-              <BiHomeAlt />
+          <div className={iconStyle}>
+            <NavLink to="/" activeClass="active">
+              <MdHomeFilled />
             </NavLink>
           </div>
-          <div className={IconStyle}>
-            <NavLink
-              to='/about'
-              activeClass='active'
-              className='cursor-pointer flex items-center justify-center hover:text-white'>
-              <BiUser />
+          <div className={iconStyle}>
+            <NavLink to="/about" activeClass="active">
+              <BiSolidUser />
             </NavLink>
           </div>
-          <div className={IconStyle}>
-            <NavLink
-              to='/work'
-              activeClass='active'
-              className='cursor-pointer flex items-center justify-center hover:text-white'>
-              <BsBriefcase />
+          <div className={iconStyle} >
+            <NavLink to="/work" activeClass="active">
+              <BsBriefcaseFill />
             </NavLink>
           </div>
-          <div className={IconStyle}>
-            <NavLink
-              to='/contact'
-              activeClass='active'
-              className='cursor-pointer flex items-center justify-center hover:text-white'>
-              <LuContact />
+          <div className={iconStyle}>
+            <NavLink to="/contact" activeClass="active">
+              <BsFillPersonVcardFill />
             </NavLink>
           </div>
-          <NavLink to="https://www.linkedin.com/in/omkarkarale541/" className='btn text-sm cursor-pointer px-4 py-[7px] max-sm:hidden font-bold'>
+          <NavLink to="https://www.linkedin.com/in/omkarkarale541/" className='btn text-sm cursor-pointer px-4 py-[7px] max-sm:hidden font-extrabold'>
             <span className='animate-pulse'>Hire Me ✨</span>
           </NavLink>
         </div>
       </div>
     </nav>
-  )
-}
-
-export default NavBar
+  );
+};
+export default NavBar;
